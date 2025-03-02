77365817de89155b880d0492bfa9e53b63b2a4bd
jirmskie9 <jirmskie9@gmail.com>
2025-03-02 14:33:59 +0800

    test

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost"; 
 $user = "u507130350_johnrid"; 
-$pass = "/0pXU13n*pc"; 
+$pass = "Johnrid123"; 
 $db_name = "u507130350_board"; 
 
 $conn = new mysqli($host, $user, $pass, $db_name);
